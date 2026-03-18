6427f48ba5076c3ba38bafb5843afbb1fa5db94d
João Teodoro <joao.v.teodoro@usp.br>
2026-03-18 19:59:59 -0300

    Change code directory

---
+++ src/hdl/ip_cores/signals_rom_ip_inst.v
@@ -0,0 +1,5 @@
+signals_rom_ip	signals_rom_ip_inst (
+	.address ( address_sig ),
+	.clock ( clock_sig ),
+	.q ( q_sig )
+	);
---
+++ src/hdl/ip_cores/twrom_inst.v
@@ -0,0 +1,5 @@
+twrom	twrom_inst (
+	.address ( address_sig ),
+	.clock ( clock_sig ),
+	.q ( q_sig )
+	);
